fc9a4579b65f70476c65e8fa1c9373279e613d23
simonass <simonas.stipinas@programmatus.com>
2019-11-26 13:23:40 +0200

    Tree hash enum and traits

--- utils/mif_tree_hash/src/lib.rs
+++ utils/mif_tree_hash/src/lib.rs
@@ -8,3 +8,71 @@ pub const MERKLE_HASH_CHUNK: usize = 2 * BYTES_PER_CHUNK;
 pub fn merkle_root(bytes: &[u8]) -> Vec<u8> {
     merkleize(bytes)
 }
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TreeHashType {
+    Basic,
+    Vector,
+    List,
+    Container,
+}
+
+pub trait TreeHash {
+    fn tree_hash_type() -> TreeHashType;
+
+    fn tree_hash_packed_encoding(&self) -> Vec<u8>;
+
+    fn tree_hash_packing_factor() -> usize;
+
+    fn tree_hash_root(&self) -> Vec<u8>;
+}
+
+pub trait SignedRoot: TreeHash {
+    fn signed_root(&self) -> Vec<u8>;
+}
+
+#[macro_export]
+macro_rules! tree_hash_ssz_encoding_as_list {
+    ($type: ident) => {
+        impl tree_hash::TreeHash for $type {
+            fn tree_hash_type() -> tree_hash::TreeHashType {
+                tree_hash::TreeHashType::List
+            }
+
+            fn tree_hash_packed_encoding(&self) -> Vec<u8> {
+                unreachable!("List should never be packed.")
+            }
+
+            fn tree_hash_packing_factor() -> usize {
+                unreachable!("List should never be packed.")
+            }
+
+            fn tree_hash_root(&self) -> Vec<u8> {
+                ssz::ssz_encode(self).tree_hash_root()
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! tree_hash_ssz_encoding_as_vector {
+    ($type: ident) => {
+        impl tree_hash::TreeHash for $type {
+            fn tree_hash_type() -> tree_hash::TreeHashType {
+                tree_hash::TreeHashType::Vector
+            }
+
+            fn tree_hash_packed_encoding(&self) -> Vec<u8> {
+                unreachable!("Vector should never be packed.")
+            }
+
+            fn tree_hash_packing_factor() -> usize {
+                unreachable!("Vector should never be packed.")
+            }
+
+            fn tree_hash_root(&self) -> Vec<u8> {
+                tree_hash::merkle_root(&ssz::ssz_encode(self))
+            }
+        }
+    };
+}
