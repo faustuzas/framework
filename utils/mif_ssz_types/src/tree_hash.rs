18ffeb7a7aa6e330273df568e8419cf8f848b1ac
Faustas <faustas.butkus@gmail.com>
2019-11-26 17:30:33 +0200

    Tree hash for vec/list signatures added

---
+++ utils/mif_ssz_types/src/tree_hash.rs
@@ -0,0 +1,7 @@
+pub fn vec_tree_hash_root<T: TreeHash, N: Unsigned>(vec: &[T]) -> Vec<u8> {
+    unimplemented!();
+}
+
+pub fn bitfield_bytes_tree_hash_root<N: Unsigned>(bytes: &[u8]) -> Vec<u8> {
+    unimplemented!();
+}
